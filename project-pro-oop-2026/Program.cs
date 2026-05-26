a46288937a3e4bfdf548fd30eb5c001bc85ef9f6
Jarne Vanwalleghem Huysmans <jarne.vanwalleghemhu@fluxus.nu>
2026-05-26 10:54:16 +0200

    Start code verwijderen en code examen plaken

--- project-pro-oop-2026/Program.cs
+++ project-pro-oop-2026/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace project_pro_oop_2026
+﻿using System;
+
+namespace Solution_Scrabble
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // VARIABELEN
+            string input = "";
+            int totaalAantalKarakters = 0;
+            int totaalWaardeKarakters = 0;
+
+            // INPUT
+            Console.WriteLine("Geef een woord of zin in:");
+            input = Console.ReadLine();
+
+            // BASIS
+            foreach (char c in input)
+            {
+                totaalAantalKarakters++;
+            }
+
+            Console.WriteLine("Aantal karakters: " + totaalAantalKarakters);
+
+            // UITBREIDING
+            Random random = new Random();
+
+            foreach (char c in input)
+            {
+                totaalWaardeKarakters += random.Next(0, 26);
+            }
+
+            Console.WriteLine("Totale waarde: " + totaalWaardeKarakters);
         }
     }
 }
