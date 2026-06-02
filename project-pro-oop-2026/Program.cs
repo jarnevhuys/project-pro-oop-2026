b8eeff1f42125b8f84c57008cf0fd729bf91c2ec
Jarne Vanwalleghem Huysmans <jarne.vanwalleghemhu@fluxus.nu>
2026-06-02 15:56:54 +0200

    Hoofdletters aangepast waar nodig

--- project-pro-oop-2026/Program.cs
+++ project-pro-oop-2026/Program.cs
@@ -11,28 +11,28 @@ namespace Solution_Scrabble
             try
             {
                 //OBJECT AANMAKEN
-                Woord Woord = new Woord();
+                Woord woord = new Woord();
 
                 // INPUT
                 Console.WriteLine("Geef een woord of zin in:");
-                Woord.input = Console.ReadLine();
+                woord.Input = Console.ReadLine();
 
                 //THROW
-                if (Woord.input == "")
+                if (woord.Input == "")
                 {
                     throw new Exception("Je moet een woord ingeven.");
                 }
 
                 //AANROEPEN METHODES/FUNCTIES (BASIS)
-                Woord.BerekenKarakters(Woord.input, Woord.totaalAantalKarakters);
+                woord.BerekenKarakters(woord.Input, woord.TotaalAantalKarakters);
 
                 //AANROEPEN METHODES/FUNCTIES (UITBREIDING)
-                Woord.BerekenWaarde(Woord.input, Woord.totaalWaardeKarakters);
+                woord.BerekenWaarde(woord.Input, woord.TotaalWaardeKarakters);
 
                 //POLYMORFISME AANROEPEN
                 Console.WriteLine("Kies mode: normal / hard / extreme");
                 string mode = Console.ReadLine();
-                Woord.BerekenWaarde(Woord.input, Woord.totaalWaardeKarakters, mode);
+                woord.BerekenWaarde(woord.Input, woord.TotaalWaardeKarakters, mode);
             }
             catch (Exception fout)
             {
@@ -45,80 +45,80 @@ namespace Solution_Scrabble
 //INTERFACE (ABSTRACTIE)
 interface iKarakterTeller
 {
-    void BerekenKarakters(string input, int totaalAantalKarakters);
+    void BerekenKarakters(string Input, int TotaalAantalKarakters);
     //NORMALE BEREKENING
-    void BerekenWaarde(string input, int totaalWaardeKarakters);
+    void BerekenWaarde(string Input, int TotaalWaardeKarakters);
 
     //POLYMORFISME BEREKENING (OVERLOADING)
-    void BerekenWaarde(string input, int totaalWaardeKarakters, string mode);
+    void BerekenWaarde(string Input, int TotaalWaardeKarakters, string Mode);
 }
 
 //KLASSEN (OVERERVING) (ACCESSOR, MUTATOR (GET, SET)) (ENCAPSULATIE)
 public class Tekst
 {
-    public string input { get; set; }
+    public string Input { get; set; }
 }
 //KLASSEN
 public class Woord : Tekst, iKarakterTeller
 {
     //ACCESSOR, MUTATOR (GET, SET) (ENCAPSULATIE)
-    public int totaalAantalKarakters { get; set; }
-    public int totaalWaardeKarakters { get; set; }
+    public int TotaalAantalKarakters { get; set; }
+    public int TotaalWaardeKarakters { get; set; }
 
     // CONSTRUCTOR
     public Woord()
     {
-        input = "";
-        totaalAantalKarakters = 0;
-        totaalWaardeKarakters = 0;
+        Input = "";
+        TotaalAantalKarakters = 0;
+        TotaalWaardeKarakters = 0;
 
         Console.WriteLine("Spel opgestart!");
     }
 
     //METHODES/FUNCTIES
-    public void BerekenKarakters(string input, int totaalAantalKarakters)
+    public void BerekenKarakters(string Input, int TotaalAantalKarakters)
     {
-        foreach (char c in input)
+        foreach (char c in Input)
         {
-            totaalAantalKarakters++;
+            TotaalAantalKarakters++;
         }
 
-        Console.WriteLine("Aantal karakters: " + totaalAantalKarakters);
+        Console.WriteLine("Aantal karakters: " + TotaalAantalKarakters);
     }
 
     //METHODES/FUNCTIES
-    public void BerekenWaarde(string input, int totaalWaardeKarakters)
+    public void BerekenWaarde(string Input, int TotaalWaardeKarakters)
     {
         Random random = new Random();
 
-        foreach (char c in input)
+        foreach (char c in Input)
         {
-            totaalWaardeKarakters += random.Next(0, 26);
+            TotaalWaardeKarakters += random.Next(0, 26);
         }
 
-        Console.WriteLine("Totale waarde: " + totaalWaardeKarakters);
+        Console.WriteLine("Totale waarde: " + TotaalWaardeKarakters);
     }
 
     //POLYMORFISME (OVERLOADING)
-    public void BerekenWaarde(string input, int totaalWaardeKarakters, string mode)
+    public void BerekenWaarde(string Input, int TotaalWaardeKarakters, string Mode)
     {
         Random random = new Random();
 
-        foreach (char c in input)
+        foreach (char c in Input)
         {
-            totaalWaardeKarakters += random.Next(0, 26);
+            TotaalWaardeKarakters += random.Next(0, 26);
         }
 
-        if (mode == "hard")
+        if (Mode == "hard")
         {
-            totaalWaardeKarakters *= 2;
+            TotaalWaardeKarakters *= 2;
         }
-        else if (mode == "extreme")
+        else if (Mode == "extreme")
         {
-            totaalWaardeKarakters *= 3;
+            TotaalWaardeKarakters *= 3;
         }
-        Console.WriteLine("Mode: " + mode);
-        Console.WriteLine("Totale waarde:" + totaalWaardeKarakters);
+        Console.WriteLine("Mode: " + Mode);
+        Console.WriteLine("Totale waarde:" + TotaalWaardeKarakters);
     }
 
 }
